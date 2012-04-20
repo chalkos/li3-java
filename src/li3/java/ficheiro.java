86f9375590bcf9b34f332b2fddfe5caae52ee5f8
Bruno Ferreira <chalkos@chalkos.net>
2012-04-20 15:30:47 +0100

    adicionados ficheiros de dados de teste

--- src/li3/java/ficheiro.java
+++ src/li3/java/ficheiro.java
@@ -1,5 +1,9 @@
 package li3.java;
 
 public class ficheiro {
+    private static final String nome_ficheiro_utilizadores = "sample_utilizadores.txt";
+    private static final String nome_ficheiro_localidades = "sample_localidades.txt";
+    private static final String nome_ficheiro_ligacoes = "sample_ligacoes.txt";
+    
     
 }
