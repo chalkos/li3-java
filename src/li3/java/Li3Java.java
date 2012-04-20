28e18d70423820044bb13fb36a817f83883a8244
Bruno Ferreira <chalkos@chalkos.net>
2012-04-20 12:03:45 +0100

    Modificado LI3Java.java

--- src/li3/java/Li3Java.java
+++ src/li3/java/Li3Java.java
@@ -3,7 +3,7 @@ package li3.java;
 public class Li3Java {
 
     /**
-     * @param args the command line arguments
+     * @param args argumentos da linha de comandos
      */
     public static void main(String[] args) {
         System.out.println("It's alive!");
