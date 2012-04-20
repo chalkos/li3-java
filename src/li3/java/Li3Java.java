d4ec6fdf76669a96954920f44a00076a50ee0416
Bruno Ferreira <chalkos@chalkos.net>
2012-04-20 11:48:32 +0100

    teste. isto será a main class

---
+++ src/li3/java/Li3Java.java
@@ -0,0 +1,11 @@
+package li3.java;
+
+public class Li3Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("It's alive!");
+    }
+}
