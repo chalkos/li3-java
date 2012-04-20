da150f0e3204663fdd688b780fc1e526fe148158
Bruno Ferreira <chalkos@chalkos.net>
2012-04-20 15:04:59 +0100

    Modificada main e adicionada classe para ler de ficheiros

--- src/li3/java/Li3Java.java
+++ src/li3/java/Li3Java.java
@@ -6,6 +6,10 @@ public class Li3Java {
      * @param args argumentos da linha de comandos
      */
     public static void main(String[] args) {
-        System.out.println("It's alive!");
+        //TODO: ler utilizadores do ficheiro
+        
+        //TODO: inserir um novo utilizador
+        
+        //TODO: procurar um utilizador por nome e nif
     }
 }
---
+++ src/li3/java/ficheiro.java
@@ -0,0 +1,5 @@
+package li3.java;
+
+public class ficheiro {
+    
+}
