75bcc2aafdfd04a411a26b480f8291a5c35aab9f
Bruno Ferreira <chalkos@chalkos.net>
2012-04-26 20:48:09 +0100

    ja faz a estatistica com a estrutura de dados pedida no ponto 1 dos utilizadores

--- src/li3/java/Li3Java.java
+++ src/li3/java/Li3Java.java
@@ -1,41 +1,89 @@
 package li3.java;
 
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.GregorianCalendar;
 import java.util.Iterator;
 
+
 public class Li3Java {
     private static GregorianCalendar timer;
+    private static StringBuilder estatisticas;
     
     /**
      * @param args argumentos da linha de comandos
      */
     public static void main(String[] args) {
-        StringBuilder estatisticas = new StringBuilder("Estatisticas:\n");
-        estatisticas.append("--Utilizadores (2 ArrayLists)--\n\tMostrar 5000 utilizadores por nome e por nif: ");
-        startTimer();
-        utilizadoresArrayList(5000);
-        estatisticas.append(stopTimer());
+        Integer quantidades[] = {5000, 10000, 15000, 18000};
+        
+        estatisticas = new StringBuilder("Estatisticas de Utilizadores:\n");
+        estatisticas.append("                               | Ler  | Inserir | P. Nome | P. Nif | Imprimir |\n");
+        estatisticas.append("-------------------------------+------+---------+---------+--------+----------|\n");
+        
+        for( int quantidade : quantidades ){
+            estatisticas.append(String.format(" 2 ArrayLists (%5d)          |", quantidade));
+            utilizadoresArrayList(quantidade);
+        }
+        estatisticas.append("-------------------------------+------+---------+---------+--------+----------+\n");
         System.out.print(estatisticas);
     }
     
     public static void utilizadoresArrayList(int numDados){
+        Iterator itr;
+        int comparacao = new Integer(-1);
+        
         //recolher os dados
+        startTimer();
         ArrayList<Utilizador> usersNome;
         ArrayList<Utilizador> usersNif;
         usersNif = ficheiro.getUtilizadoresArrayList(numDados);
         usersNome = (ArrayList<Utilizador>)usersNif.clone();
-        
-        //ordenar os dados
-        //Collection.sort(usersNif);
-        
         Collections.sort(usersNif,new comparadorNif());
         Collections.sort(usersNome, new comparadorNome());
+        estatisticas.append(String.format(" %4d |", stopTimer()));
         
+        //inserir um novo registo
+        startTimer();
+        usersNif.add(new Utilizador(123456789, "Joaquim", "Rua das flores"));
+        usersNome = (ArrayList<Utilizador>)usersNif.clone();
+        Collections.sort(usersNif,new comparadorNif());
+        Collections.sort(usersNome, new comparadorNome());
+        estatisticas.append(String.format(" %7d |", stopTimer()));
         
-        //escrever as informações
-        Iterator itr = usersNif.iterator();
+        //procurar por nome
+        startTimer();
+        Utilizador procura = new Utilizador("Não existe");
+        for( Utilizador tmp : usersNome ){
+            comparacao = procura.compareNome(tmp);
+            if( comparacao >= 0 )
+                break;
+        }
+        
+        if( comparacao == 0 )
+            System.out.println("existe");
+        else
+            System.out.println("não existe");
+        estatisticas.append(String.format(" %7d |", stopTimer()));
+        
+        //procurar por nif
+        startTimer();
+        procura = new Utilizador(123456789);
+        for( Utilizador tmp : usersNif ){
+            comparacao = procura.compareNif(tmp);
+            if( comparacao >= 0 )
+                break;
+        }
+        
+        if( comparacao == 0 )
+            System.out.println("existe");
+        else
+            System.out.println("não existe");
+        estatisticas.append(String.format(" %6d |", stopTimer()));
+        
+        //imprimir os dados dos utilizadores
+        startTimer();
+        itr = usersNif.iterator();
         
         System.out.println("Por nif:");
         while(itr.hasNext())
@@ -45,6 +93,74 @@ public class Li3Java {
         System.out.println("Por nome:");
         while(itr.hasNext())
             System.out.println(itr.next().toString());
+        estatisticas.append(String.format(" %8d |\n", stopTimer()));
+    }
+    
+    public static void utilizadoresArrayLinked(int numDados){
+        Iterator itr;
+        int comparacao = new Integer(-1);
+        
+        //recolher os dados
+        startTimer();
+        ArrayList<Utilizador> usersNome;
+        ArrayList<Utilizador> usersNif;
+        usersNif = ficheiro.getUtilizadoresArrayList(numDados);
+        usersNome = (ArrayList<Utilizador>)usersNif.clone();
+        Collections.sort(usersNif,new comparadorNif());
+        Collections.sort(usersNome, new comparadorNome());
+        estatisticas.append(String.format(" %4d |", stopTimer()));
+        
+        //inserir um novo registo
+        startTimer();
+        usersNif.add(new Utilizador(123456789, "Joaquim", "Rua das flores"));
+        usersNome = (ArrayList<Utilizador>)usersNif.clone();
+        Collections.sort(usersNif,new comparadorNif());
+        Collections.sort(usersNome, new comparadorNome());
+        estatisticas.append(String.format(" %7d |", stopTimer()));
+        
+        //procurar por nome
+        startTimer();
+        Utilizador procura = new Utilizador("Não existe");
+        for( Utilizador tmp : usersNome ){
+            comparacao = procura.compareNome(tmp);
+            if( comparacao >= 0 )
+                break;
+        }
+        
+        if( comparacao == 0 )
+            System.out.println("existe");
+        else
+            System.out.println("não existe");
+        estatisticas.append(String.format(" %7d |", stopTimer()));
+        
+        //procurar por nif
+        startTimer();
+        procura = new Utilizador(123456789);
+        for( Utilizador tmp : usersNif ){
+            comparacao = procura.compareNif(tmp);
+            if( comparacao >= 0 )
+                break;
+        }
+        
+        if( comparacao == 0 )
+            System.out.println("existe");
+        else
+            System.out.println("não existe");
+        estatisticas.append(String.format(" %6d |", stopTimer()));
+        
+        //imprimir os dados dos utilizadores
+        startTimer();
+        itr = usersNif.iterator();
+        
+        System.out.println("Por nif:");
+        while(itr.hasNext())
+            System.out.println(itr.next().toString());
+        
+        itr = usersNome.iterator();
+        System.out.println("Por nome:");
+        while(itr.hasNext())
+            System.out.println(itr.next().toString());
+        estatisticas.append(String.format(" %8d |\n", stopTimer()));
     }
     
     /**
@@ -60,6 +176,6 @@ public class Li3Java {
      */
     public static long stopTimer(){
         GregorianCalendar now = new GregorianCalendar();
-        return timer.getTimeInMillis()-now.getTimeInMillis();
+        return now.getTimeInMillis()-timer.getTimeInMillis();
     }
 }
